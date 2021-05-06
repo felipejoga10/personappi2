020123f626d735af9a87cea6ffaa33f5320d1964
felipejoga10 <bavaresco3@yahoo.com.br>
2021-05-06 10:08:29 -0300

    make it better

---
+++ src/main/java/dio/personappi/PersonappiApplication.java
@@ -0,0 +1,13 @@
+package dio.personappi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonappiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonappiApplication.class, args);
+	}
+
+}
